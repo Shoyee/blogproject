2beee2c49aff76a724e3b96b3c9d5c78f93e9bd0
shoyee <shuoyu_2008@163.com>
2018-07-21 12:17:03 +0800

    modified two fields of class_Post

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils.six import python_2_unicode_compatible
+import pytz
+from django.utils.timezone import now
 
 # Create your models here.
 
@@ -25,12 +27,13 @@ class Tag(models.Model):
 
 
 # 创建博客文章数据表
+tz = pytz.timezone('Asia/Shanghai')
 @python_2_unicode_compatible
 class Post(models.Model):
     title = models.CharField(max_length=100, help_text='文章标题')
     body = models.TextField(help_text='文章正文')
-    created_time = models.DateTimeField(help_text='创建时间')
-    modified_time = models.DateTimeField(help_text='最后修改时间')
+    created_time = models.DateTimeField(default=now(), help_text='创建时间')
+    modified_time = models.DateTimeField(default=now(), help_text='最后修改时间')
     excerpt = models.CharField(max_length=200, blank=True, help_text='文章摘要')
     category = models.ForeignKey(Category, help_text='文章的分类')
     tags = models.ManyToManyField(Tag, blank=True, help_text='文章的标签')
