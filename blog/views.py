9ac0ccb2f68a3830cb78b8f5b817c2963d90fb87
shoyee <shuoyu_2008@163.com>
2018-07-23 14:21:04 +0800

    modified the pagination view and made it better

--- blog/views.py
+++ blog/views.py
@@ -15,6 +15,104 @@ class IndexView(ListView):
     # 实现分页功能，每页展示5篇文章
     paginate_by = 5
 
+    # 在类视图中，需要传递的模板变量字典是通过 get_context_data 获得的，
+    # 所以我们复写该方法，以便我们能够自己再插入一些我们自定义的模板变量进去。
+    def get_context_data(self, **kwargs):
+        # 获取父类生成的传递给模板的字典
+        context = super().get_context_data(**kwargs)
+        # 父类生成的字典中，已经有了paginator,page_obj,is_paginated这三个模板变量了，直接获取就可以
+        paginator = context.get('paginator')
+        page = context.get('page_obj')
+        is_paginated = context.get('is_paginated')
+
+        # 调用pagination_data方法获取分页导航所需要的数据
+        pagination_data = self.pagination_data(paginator, page, is_paginated)
+
+        # pagination_data返回的也是一个字典，将返回的信息更新到context中
+        context.update(pagination_data)
+
+        # 返回更新后的context,以便ListView使用context中的模板变量去渲染模板
+        return context
+
+    # 定义分页数据函数
+    def pagination_data(self, paginator, page, is_paginated):
+        if not is_paginated:
+            # 如果没有分页，则返回空字典
+            return {}
+
+        # 设置当前页面的左边和右边连续的页码号，初始值为空
+        left = []
+        right = []
+        # 标示第1页页码后面和最后一页页码前面是否需要显示为省略号
+        left_has_more = False
+        right_has_more = False
+        # 标示是否需要显示第1页和最后一页的页码号，默认为False
+        first = False
+        last = False
+        # 获取用户请求的当前页码
+        page_number = page.number
+        # 获取总页码
+        total_pages = paginator.num_pages
+        # 获取整个分页页码的列表
+        page_range = paginator.page_range
+
+        if page_number ==1:
+            # 如果用户获取的当前页码是第一页面，则把右边的连续页码获取为显示两页，左边不变，依然为空
+            right = page_range[page_number:page_number + 2]
+
+            if right[-1] < total_pages - 1:
+                # 如果当前页码右边连续显示的页码列表内的最右边的页码比总页码减1还要小，说明右边到最后页码的中间还有页码，就要用省略号来显示
+                # 可以通过right_has_more来控制
+                right_has_more = True
+
+            if right[-1] < total_pages:
+                # 如果当前页码右边连续显示的页码列表内的最右边的页码比总页码小，则说明右侧连续页码里不包含最后一页，则需要显示最后一页的页码
+                # 可以通过last来控制
+                last = True
+
+        elif page_number == total_pages:
+            # 如果用户获取的当前页面是最后一页，那么当前页面的右侧连续页面为空，左边设置显示连续两页
+            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
+
+            if left[0] > 2:
+                # 如果当前页面左边连续显示的页面列表的第一个页码比2大，则说明从第一页到左侧显示的页面之间还有页面，需要显示省略号
+                # 可以通过left_has_more来控制
+                left_has_more =True
+
+            if left[0] > 1:
+                # 如果如果当前页面左边连续显示的页面列表的第一个页码比1大，说明最左侧的页码不是第一页，需要显示第一页的页码
+                # 可以通过first来控制
+                first = True
+        else:
+            # 如果用户请求的当前页面既不是第一页，也不是最后一页,则设置当前页面的左边和右边显示的连续页码为2个页码
+            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
+            right = page_range[page_number:page_number + 2]
+
+            # 是否显示最后一页和最后一页前面的省略号
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+
+            # 是否显示第一页和第一页后面的省略号
+            if left[0] > 2:
+                left_has_more = True
+            if left[0] > 1:
+                first = True
+
+        # 生成分页信息模板变量字典
+        data = {
+            'left': left,
+            'right': right,
+            'left_has_more': left_has_more,
+            'right_has_more': right_has_more,
+            'first': first,
+            'last': last,
+        }
+
+        # 返回分页信息
+        return data
+
 # 文章详情页面函数
 # def detail(request, pk):
 #     post = get_object_or_404(Post, pk=pk)
