5e2787123d3362a497b811ad930fc018ee6e9514
shoyee <shuoyu_2008@163.com>
2018-08-03 20:23:13 +0800

    modified the wrong word of cache location

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 CACHE = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATE': 'redis://127.0.0.1:6379/1',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
             'PASSWORD': '1990115',
